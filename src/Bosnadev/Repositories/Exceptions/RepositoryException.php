21551f6092e0426985ce923cba18ce7484b4a5f8
Mirza Pasic <mirza.pasic@bosnadev.com>
2016-08-19 16:31:15 +0200

    update

--- src/Bosnadev/Repositories/Exceptions/RepositoryException.php
+++ src/Bosnadev/Repositories/Exceptions/RepositoryException.php
@@ -6,4 +6,7 @@ namespace Bosnadev\Repositories\Exceptions;
  * Class RepositoryException
  * @package Bosnadev\Repositories\Exceptions
  */
-class RepositoryException extends \Exception {}
+class RepositoryException extends \Exception
+{
+
+}
